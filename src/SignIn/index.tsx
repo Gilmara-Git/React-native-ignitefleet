872dde46d57df28bc268aa1f7fe348bf32d97c32
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-02-24 21:07:01 -0500

    initiate the project

---
+++ src/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import {styles} from './styles';
+
+export const SignIn = ()=>{
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Sign In page
+            </Text>
+        </View>
+    )
+};
