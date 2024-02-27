359d95fc5cb6dfda720c9c7286c23b4198af9e77
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-02-27 08:32:13 -0500

    feat: add ios and android build in emulators

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { SignIn } from './src/SignIn';
+import { StatusBar } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+import theme from './src/theme';
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold  } from '@expo-google-fonts/roboto';
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env';
 
 export default function App() {
+
+  
+  const  [fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+
+  if(!fontsLoaded){
+    return (
+      <Loading />
+    )
+  }
+
   return (
-    <>
-    <StatusBar/>
-   <SignIn/>
-    </>
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+        />
+
+      <SignIn/>
+    </ThemeProvider>
   );
 }
