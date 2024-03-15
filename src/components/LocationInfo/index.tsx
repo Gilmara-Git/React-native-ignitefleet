5fb916b120b375ff341ac0a6402d7a27bd28eac4
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-03-15 19:58:31 -0400

    feat: add LocationInfo

---
+++ src/components/LocationInfo/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container, Info, Label, Description } from "./styles";
+import { useTheme } from "styled-components/native";
+import { IconBoxProps } from "../IconBox";
+import { IconBox } from '../IconBox';
+
+export type LocationInfoProps = {
+  label: string;
+  description: string;
+  icon: IconBoxProps;
+
+};
+
+type Props = LocationInfoProps;
+
+export const LocationInfo = ({ icon,  label, description }: Props) => {
+  const { COLORS } = useTheme();
+
+  return (
+    <Container>
+     <IconBox icon={icon}/>
+ 
+      <Info>
+        <Label numberOfLines={1}>{label}</Label>
+
+        <Description numberOfLines={1}>{description}</Description>
+      </Info>
+    </Container>
+  );
+};
