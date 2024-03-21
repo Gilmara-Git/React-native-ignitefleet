e3eedd9c86c81019bafb44f43c6e399d4d5ec934
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-03-20 21:09:19 -0400

    refactor: add CFBundleURLSchemes under infoPlist, so ios user can authenticate with RN Google SignIn

--- app.config.js
+++ app.config.js
@@ -1,62 +1,67 @@
-
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 module.exports = {
-  "expo": {
-    "name": "ignitefleet",
-    "slug": "ignitefleet",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "dark",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "cover",
-      "backgroundColor": "#202024"
+  expo: {
+    name: "ignitefleet",
+    slug: "ignitefleet",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "dark",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "cover",
+      backgroundColor: "#202024",
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.gilmarapq.ignitefleet",
-      "config":{
-        "googleMapsApiKey": process.env.GOOGLE_MAPS_API_KEY,
-
-      }
-
-    },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#202024"
-        
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.gilmarapq.ignitefleet",
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
       },
-      "package": "com.gilmarapq.ignitefleet",
-      "config":{
-        "googleMaps":{
-          "apiKey": process.env.GOOGLE_MAPS_API_KEY
-        }
-      }
+      infoPlist: {
+        CFBundleURLTypes: [
+          {
+            CFBundleURLSchemes: [
+              "com.googleusercontent.apps.140587143092-veeii9pfhpin345dato0uguddh41r9l3",
+            ],
+          },
+        ],
+      },
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#202024",
+      },
+      package: "com.gilmarapq.ignitefleet",
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        },
+      },
     },
-    "plugins": [
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: [
       "expo-font",
       [
         "@react-native-google-signin/google-signin",
         {
-          "iosUrlScheme": "com.googleusercontent.apps.140587143092-veeii9pfhpin345dato0uguddh41r9l3"
-        }
+          iosUrlScheme:
+            "com.googleusercontent.apps.140587143092-veeii9pfhpin345dato0uguddh41r9l3",
+        },
       ],
       [
         "expo-location",
         {
-          "locationAlwaysAndWhenInUsePermission": "Allow $(PRODUCT_NAME) to use your location."
-        }
-      ]
-    ]
-  }
-}
+          locationAlwaysAndWhenInUsePermission:
+            "Allow $(PRODUCT_NAME) to use your location.",
+        },
+      ],
+    ],
+  },
+};
